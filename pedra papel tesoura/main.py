1b54fd8a3b8a48b02daf6b999ff2ed1b1f339b39
Amador Rufino <rufinoalmeida97@gmail.com>
2025-06-06 21:18:58 -0300

    atualização
    2

---
+++ pedra papel tesoura/main.py
@@ -0,0 +1,17 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+# Cores -------------------------------
+
+co0 = "#FFFFFF"  # white
+co1 = "#333333"  # black
+co2 = "#fcc058"  # orange
+co3 = "#38576b"  # valor
+co4 = "#3297ab"  # blue
+co5 = "#fff873"  # yellow
+co6 = "#fcc058"  # orange
+co7 = "#e85151"  #vermelhor
+co8 = "#34eb3d"  # green
+fundo = "#3b3b3b" # black
+
